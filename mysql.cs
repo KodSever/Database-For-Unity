3c6174f87208e85377e3ca4771b5ad14d8ff6cf6
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-06-06 17:03:16 +0300

    Create mysql.cs

---
+++ mysql.cs
@@ -0,0 +1,53 @@
+# MYSQL NASIL KULLANILIR.
+Assembly klasorü oluştur.
+MySql.Data.dll dosyasını indir. https://www.dllme.com/dll/files/mysql_data_dll.html
+Assembly klasorünün içine MySql.Data.dll dosyasını at. 
+Usingle çağır.
+ConnectionStrings ile değerlerini gir.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+public class _veritabani : MonoBehaviour
+{
+    MySqlConnection conn;
+
+    public string server, database, user_name, pass, port;
+
+    private void Start()
+    {
+        //server = "MySQL";//Bozuk
+        //server = ".";//Bozuk
+
+        //server = "127.0.0.1"; //Sağlam
+        server = "localhost"; //Sağlam
+
+        database = "dataBase1";
+        user_name = "root";
+        pass = "";
+
+        port = "3306";
+
+        
+    }
+    void setConnection()
+    {
+        conn = new MySqlConnection();
+        conn.ConnectionString = "Server=" + server + ";Port=" + port + ";Database=" + database + ";Uid=" + user_name + ";Pwd=" + pass + ";";
+        //conn.ConnectionString = "Server="+server+";Database="+database+";Uid="+user_name+";Pwd="+pass+";";
+        try
+        {
+            conn.Open();
+            Debug.Log("Bağlantı Sağlandı! " + conn.State);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Hata: " + ex.Message);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){ setConnection(); }
+    }
+}
